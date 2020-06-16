9154bfa21cc814fc93acc7850ae1324235fab35e
didier <didier@d-defrance.fr>
2020-06-16 11:48:38 +0200

    s

--- tpJava/src/tp/MyApp.java
+++ tpJava/src/tp/MyApp.java
@@ -3,7 +3,20 @@ package tp;
 public class MyApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world 3 le retour");
+		System.out.println("Hello world");
+		m1();
+		//m2();
+	}
+	
+	public static void m1() {
+		String s1="12.5";
+		String s2="3.5";
+		double v1,v2,res;
+		// calculer addition et afficher le resultat
+		v1=Double.parseDouble(s1);
+		v2=Double.parseDouble(s2);
+		res=v1+v2;
+		System.out.println("res="+res);
 	}
 
 }
