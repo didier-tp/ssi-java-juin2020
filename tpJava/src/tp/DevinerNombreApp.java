1590c9e8c480593ab0103d8422e9148078426e5d
didier <didier@d-defrance.fr>
2020-06-17 14:49:27 +0200

    s

---
+++ tpJava/src/tp/DevinerNombreApp.java
@@ -0,0 +1,15 @@
+package tp;
+
+public class DevinerNombreApp {
+
+	public static void main(String[] args) {
+		// jeux: faire deviner un nombre
+		int nombreAdeviner = (int)(100 * Math.random());
+		//MyUtil.display("nombreAdeviner="+nombreAdeviner);
+        int valeurProposee ;
+        int nbEssais = 0;
+		//TP , via une boucle while , demande à l'utilisateur de saisir un nombre 
+		//puis afficher , la valeur à deviner est plus petite ou plus grande.
+	}
+
+}
