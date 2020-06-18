22c254cab8426c4e70028545726a4dafb8f03ca8
didier <didier@d-defrance.fr>
2020-06-18 16:27:23 +0200

    debut collection

---
+++ tpJava/src/tp/TestCollectionApp.java
@@ -0,0 +1,16 @@
+package tp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCollectionApp {
+
+	public static void main(String[] args) {
+		List liste1 = new ArrayList();
+		liste1.add("lundi");
+		liste1.add("mardi");
+		
+        System.out.println("liste1="+liste1);
+	}
+
+}
