6b9c916996c3627ce6bf9e5a6203da23f711ddaf
didier <didier@d-defrance.fr>
2020-06-19 09:36:48 +0200

    s

--- tpJava/src/tp/TestCollectionApp.java
+++ tpJava/src/tp/TestCollectionApp.java
@@ -1,11 +1,28 @@
 package tp;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TestCollectionApp {
 
+	public static void printCollection(Collection<String> col){
+		for(String e : col) {
+			System.out.println("e="+e);
+		}
+	}
+	
 	public static void main(String[] args) {
+		Set<String> set1 = new HashSet<String>();
+		set1.add("France");
+		set1.add("Espagne");
+		//set1.add("France"); //ligne inutile car France déjà ajouté
+		printCollection(set1);
+		
+		//List<String> liste1 = new LinkedList<String>();
+		//List<Object> listeChoses = new ArrayList<Object>();
 		List<String> liste1 = new ArrayList<String>();
 		liste1.add("lundi");
 		liste1.add("mardi");
@@ -16,15 +33,19 @@ public class TestCollectionApp {
 		System.out.println("nb elts de liste1="+ liste1.size());
         //liste1.clear();
 		
-		for(String jour : liste1) {
+		printCollection(liste1);
+		/*for(String jour : liste1) {
 			System.out.println("jour="+jour);
-		}
+		}*/
 		
-        System.out.println("liste1="+liste1);
+        System.out.println("liste1="+liste1.toString());
         
         
         List<Integer> liste2 = new ArrayList<Integer>();
-        liste2.add(10); liste2.add(20);
+        //List<int> liste2Bis = new ArrayList<int>(); IMPOSSIBLE car int n'est pas un cas particulier de Object
+        //liste2.add(new Integer(10));
+        liste2.add(10); 
+        liste2.add(20);
         for(Integer val : liste2) {
 			System.out.println("val="+val);
 		}
