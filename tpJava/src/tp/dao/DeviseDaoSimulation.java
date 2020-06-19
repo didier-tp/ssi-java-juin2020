aedffead3c567d1f71cdf9e05e560a57f33dacd6
didier <didier@d-defrance.fr>
2020-06-19 12:01:28 +0200

    debut map

--- tpJava/src/tp/dao/DeviseDaoSimulation.java
+++ tpJava/src/tp/dao/DeviseDaoSimulation.java
@@ -1,10 +1,24 @@
 package tp.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import tp.data.Devise;
 
 public class DeviseDaoSimulation implements DeviseDao {
+	
+	//table d'association entre des codes de monnaies et des objets Devises
+	private Map<String,Devise> mapDevises = new HashMap<>();
+	
+
+	public DeviseDaoSimulation() {
+		super();
+		this.mapDevises.put("EUR",new Devise("EUR","Euro",1.0) );
+		this.mapDevises.put("USD",new Devise("USD","Dollar",1.1));
+		this.mapDevises.put("GBP",new Devise("GBP","Livre",0.9));
+		this.mapDevises.put("JPY",new Devise("JPY","Yen",120.0) );
+	}
 
 	@Override
 	public Devise creerDevise(Devise d) {
@@ -14,8 +28,7 @@ public class DeviseDaoSimulation implements DeviseDao {
 
 	@Override
 	public Devise rechercherDevise(String code) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.mapDevises.get(code);
 	}
 
 	@Override
--- tpJava/src/tp/dao/TestDaoApp.java
+++ tpJava/src/tp/dao/TestDaoApp.java
@@ -1,9 +1,14 @@
 package tp.dao;
 
+import tp.data.Devise;
+
 public class TestDaoApp {
 
 	public static void main(String[] args) {
-
+        DeviseDao deviseDao = null; //DeviseDao est une interface
+        deviseDao = new DeviseDaoSimulation();
+        Devise deviseDollar = deviseDao.rechercherDevise("USD");
+        System.out.println("deviseDollar= "+deviseDollar);
 	}
 
 }
