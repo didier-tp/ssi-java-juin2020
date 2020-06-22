d9f3f8fe13885356336689970709d065324eab30
didier <didier@d-defrance.fr>
2020-06-22 16:41:27 +0200

    s

---
+++ tpJavaMaven/src/main/java/tp/IntrospectionApp.java
@@ -0,0 +1,24 @@
+package tp;
+
+import java.lang.reflect.Field;
+
+public class IntrospectionApp {
+
+	public static void main(String[] args) {
+		String nomClasse="tp.data.Produit";
+		//String nomClasse="java.text.SimpleDateFormat";
+		try {
+			Class c = Class.forName(nomClasse); //c de type java.lang.Class (méta classe)
+			//est un objet technique decruit la structure d'une classe java ordinaire
+			Field[] tabChamps = c.getDeclaredFields();
+			for(Field f : tabChamps) {
+				System.out.println("attribut " + f.getName() );
+			}
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
