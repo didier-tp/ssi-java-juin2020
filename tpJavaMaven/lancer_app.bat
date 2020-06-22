7299185ebc1ffc2653ab176371992029abb32203
didier <didier@d-defrance.fr>
2020-06-22 12:21:11 +0200

    .bat

--- tpJavaMaven/lancer_app.bat
+++ tpJavaMaven/lancer_app.bat
@@ -1,2 +1,5 @@
-cd /d "%dp0"
-java -classpath 
+REM open with / text editor pour saisir sous eclipse
+REM open with / system editor  pour lancer depuis eclipse
+cd /d "%~dp0"
+java -classpath target/tpJavaMaven.jar -DmonAppli.Debug=true tp.CarreApp 5
+pause
