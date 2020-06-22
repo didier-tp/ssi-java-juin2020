6b192610a0a57277895ddb89d9179fc2ab204883
didier <didier@d-defrance.fr>
2020-06-22 17:01:19 +0200

    introspection

--- tpJavaMaven/src/main/java/tp/IntrospectionApp.java
+++ tpJavaMaven/src/main/java/tp/IntrospectionApp.java
@@ -2,6 +2,8 @@ package tp;
 
 import java.lang.reflect.Field;
 
+import tp.data.Produit;
+
 public class IntrospectionApp {
 
 	public static void main(String[] args) {
@@ -16,6 +18,19 @@ public class IntrospectionApp {
 			}
 			
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("------------------");
+		try {
+			Produit prod = new Produit(999,"produit 999",99.99,20.0);
+			Class c = prod.getClass(); //meta description de la classe de l'objet prod
+			Field[] tabChamps = c.getDeclaredFields();
+			for(Field f : tabChamps) {
+				f.setAccessible(true); //pour acceder aux parties privées
+				System.out.println("attribut nom=" + f.getName() + " type=" + f.getType().getName()
+						          + " valeur=" +f.get(prod).toString());
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
