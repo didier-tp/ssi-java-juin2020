233bd8ad42be741bbc57a76781264c763a8f7460
didier <didier@d-defrance.fr>
2020-06-23 16:18:25 +0200

    s

--- tpJavaMaven/src/main/java/tp/CodeTraitement.java
+++ tpJavaMaven/src/main/java/tp/CodeTraitement.java
@@ -1,6 +1,24 @@
 package tp;
 
 public class CodeTraitement implements Runnable{
+	
+	private int coeff;
+	
+	
+
+	public CodeTraitement(int coeff) {
+		super();
+		this.coeff = coeff;
+	}
+
+
+
+	public CodeTraitement() {
+		super();
+		this.coeff =1;
+	}
+
+
 
 	@Override
 	public void run() {
@@ -8,7 +26,7 @@ public class CodeTraitement implements Runnable{
 		for(int i=0;i<n;i++) {
 			System.out.println("i="+i);
 			try {
-				Thread.sleep(500); //pause de 500ms (simuler un traitement long / bloquant)
+				Thread.sleep(500*this.coeff); //pause de 500ms (simuler un traitement long / bloquant)
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
--- tpJavaMaven/src/main/java/tp/ThreadApp.java
+++ tpJavaMaven/src/main/java/tp/ThreadApp.java
@@ -6,10 +6,11 @@ public class ThreadApp {
 		// instructions executées par le thread principal (créé  d'office)
 		
 		CodeTraitement codeTraitement = new CodeTraitement();//avec .run()
+		CodeTraitement codeTraitement2 = new CodeTraitement(2);
 		
         Thread nouveauThread2 = new Thread(codeTraitement);
         Thread nouveauThread3 = new Thread(codeTraitement);
-        Thread nouveauThread4 = new Thread(codeTraitement);
+        Thread nouveauThread4 = new Thread(codeTraitement2);
         
         nouveauThread2.start(); //début d'une execution de code en //
         nouveauThread3.start();
