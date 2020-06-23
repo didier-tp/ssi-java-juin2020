0d7ab730aba2d687b782e6989b8c4ee5279c330f
didier <didier@d-defrance.fr>
2020-06-23 16:29:54 +0200

    s

--- tpJavaMaven/src/main/java/tp/ThreadApp.java
+++ tpJavaMaven/src/main/java/tp/ThreadApp.java
@@ -1,6 +1,16 @@
 package tp;
 
 public class ThreadApp {
+	
+	public static void traitementSimple() {
+		System.out.println("debut");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("fin");
+	}
 
 	public static void main(String[] args) {
 		// instructions executées par le thread principal (créé  d'office)
@@ -12,9 +22,13 @@ public class ThreadApp {
         Thread nouveauThread3 = new Thread(codeTraitement);
         Thread nouveauThread4 = new Thread(codeTraitement2);
         
+        Thread nouveauThread5 = new Thread(()-> { ThreadApp.traitementSimple();}) ;
+        //ou bien Thread nouveauThread5 = new Thread(ThreadApp::traitementSimple );
+        
         nouveauThread2.start(); //début d'une execution de code en //
         nouveauThread3.start();
         nouveauThread4.start();
+        nouveauThread5.start();
 	}
 
 }
