02be9a20799255c708fb23867f98e93eeba64bd3
didier <didier@d-defrance.fr>
2020-06-23 14:32:48 +0200

    stream ok

--- tpJavaMaven/src/main/java/tp/StreamApp.java
+++ tpJavaMaven/src/main/java/tp/StreamApp.java
@@ -1,9 +1,8 @@
 package tp;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import tp.data.Produit;
 
@@ -17,8 +16,9 @@ public class StreamApp {
         listeProduits.add(new Produit(2,"classeur",3.2,20.0));
         listeProduits.add(new Produit(3,"gomme",2.0,20.0));
         listeProduits.add(new Produit(4,"cahier",4.0,20.0));
+        listeProduits.add(new Produit(5,"regle",2.7,20.0));
 
-        System.out.println("sans tri:");
+        System.out.println("liste initiale:");
         for(Produit p : listeProduits) {
         	System.out.println("\t" +p);
         }
@@ -28,11 +28,20 @@ public class StreamApp {
         //.filter(...) avec une lambda qui filtre les produits prix >=2.5
         //.map (...)  avec une lambda qui va transformer les produits , label en majuscules
         //.collect(Collectors.toList());
+        List<Produit> listeProduitsTriesFiltresEtTransformes =
+        		listeProduits.stream()
+                 .sorted((p1,p2)->(int)(p1.getPrixHt() -p2.getPrixHt()))
+                 .filter((p)->p.getPrixHt()>=2.5)
+                 .map((p) -> { p.setLabel(p.getLabel().toUpperCase()); return p; })
+                 .collect(Collectors.toList());
         
         //exemple de lambda pour .map(...)
-        // (p) -> { p.setLabel(p.getLabel().toUppperCase()); return p; }
+        // (p) -> { p.setLabel(p.getLabel().toUpperCase()); return p; }
         
         //afficher la nouvelle liste construite.
-        
+        System.out.println("liste triée filtrée et transformée:");
+        for(Produit p : listeProduitsTriesFiltresEtTransformes) {
+        	System.out.println("\t" +p);
+        }
 	}
 }
