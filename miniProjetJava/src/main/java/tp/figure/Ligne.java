5f9e221069461e8d0274c54ef6f9ff4ea3a88e5f
didier <didier@d-defrance.fr>
2020-06-24 15:18:51 +0200

    solution miniProjetJava

---
+++ miniProjetJava/src/main/java/tp/figure/Ligne.java
@@ -0,0 +1,58 @@
+package tp.figure;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Ligne extends Figure2D {
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+	
+	public Ligne() {
+		super();
+	}
+
+
+	public Ligne(int x1, int y1, int x2, int y2,String couleur, Integer epaisseur, String couleurFond) {
+		super(couleur, epaisseur, couleurFond);
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+
+	@Override
+	public String toSvgSubString() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+		ps.printf("<line x1='%d' y1='%d' x2='%d' y2='%d'", 
+				   this.x1 , this.y1 , this.x2, this.y2);
+		return baos.toString();
+	}
+
+	@Override
+	public void translater(int dx, int dy) {
+		this.x1 +=dx;
+		this.y1 +=dy;
+		this.x2 +=dx;
+		this.y2 +=dy;
+	}
+
+	@Override
+	public void zoomer(double coeff) {
+		this.x1 =(int)(this.x1 * coeff);
+		this.y1 =(int)(this.y1 * coeff);
+		this.x2 =(int)(this.x2 * coeff);
+		this.y2 =(int)(this.y2 * coeff);
+	}
+
+	@Override
+	public String toString() {
+		return "Ligne [x1=" + x1 + ", y1=" + y1 + ", x2=" + x2 + ", y2=" + y2 + ", héritant de " + super.toString()
+				+ "]";
+	}
+	
+	
+
+}
---
+++ miniProjetJava/src/main/java/tp/figure/Rectangle.java
@@ -0,0 +1,75 @@
+package tp.figure;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Rectangle extends Figure2D implements Surface{
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	
+
+	public Rectangle() {
+		super();
+	}
+	
+
+
+
+	public Rectangle(int x, int y, int width, int height,String couleur, Integer epaisseur, String couleurFond) {
+		super(couleur, epaisseur, couleurFond);
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+
+
+
+
+	@Override
+	public String toSvgSubString() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+		ps.printf("<rect x='%d' y='%d' width='%d' height='%d'", 
+				   this.x , this.y , this.width, this.height);
+		return baos.toString();
+	}
+
+	@Override
+	public double perimetre() {
+		return 2*(width+height);
+	}
+
+	@Override
+	public double aire() {
+		return width*height;
+	}
+
+	@Override
+	public void translater(int dx, int dy) {
+		this.x += dx;
+		this.y += dy;
+		
+	}
+
+	@Override
+	public void zoomer(double coeff) {
+		this.x = (int)(this.x * coeff);
+		this.y = (int)(this.y * coeff);
+		this.width = (int)(this.width * coeff);
+		this.height = (int)(this.height * coeff);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Rectangle [x=" + x + ", y=" + y + ", width=" + width + ", height=" + height + ", heritant de "
+				+ super.toString() + "]";
+	}
+	
+	
+
+}
