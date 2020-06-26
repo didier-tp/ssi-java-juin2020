8c896f3a7db0364234f92557c17cdb51834cd963
didier <didier@d-defrance.fr>
2020-06-26 16:10:11 +0200

    tomcat servlet jsp

---
+++ lancerTomcatWithEnv.bat
@@ -0,0 +1,7 @@
+REM set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.4
+set PATH=%JAVA_HOME%\bin;%PATH%
+startup.bat
+REM si conflit de numero de port (8080 déjà pris) modifier tomcat../conf/server.xml
+REM ne pas fermer la fenetre tomcat
+REM ouvrir http://localhost:8080 via un navigateur
